2d3d66bfde0d328610824a5ae055f94f866ebbfd
wshy868 <wshy868@wshy868dembp>
2018-11-04 01:43:19 -0500

    initialize the project

---
+++ Pokemon/src/data/rdg/RDG.java
@@ -0,0 +1,8 @@
+package data.rdg;
+
+public abstract class RDG {
+	public abstract int insert();
+	public abstract int update();
+	public abstract int delete();
+	public abstract int find(long id);
+}
